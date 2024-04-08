695a8d578e39bb7bd5ca3f7f51f8654c7119a8d0
Ryan Brue <ryanbrue@hotmail.com>
2024-04-08 10:48:23 -0500

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use async_std::{fs::File, io::WriteExt, path::PathBuf};
 use tempfile::TempDir;
-use zbus::{interface, Error as ZbusError, Connection, DBusError, Result};
+use zbus::{interface, Connection, Result as ZbusResult};
 
 struct Daemon {
     path: TempDir,
@@ -10,35 +10,32 @@ struct Daemon {
 #[interface(name = "org.ryanabx.DesktopEntryDaemon")]
 impl Daemon {
     /// Register a desktop entry. Required is the `domain` name (e.g. com.ryanabx.TabletopEngine)
-    /// and the plaintext `entry`.
-    async fn register_entry(&self, domain: &str, entry: &str) -> Result<String> {
+    /// and the plaintext `entry`
+    async fn register_entry(&self, domain: &str, entry: &str) -> String {
         let file_path = self.path.path().join("my-temporary-note.txt");
-        if let Ok(mut desktop_file) = File::create(file_path).await {
-            if writeln!(desktop_file, "{}", entry).await.is_err() {
-                return Err(ZbusError::ExcessData);
-            } else {
-                return Ok("".to_string());
-            }
-        } else {
-            Err(ZbusError::ExcessData)
+        match File::create(file_path).await {
+            Ok(mut x) => match x.write(entry.as_bytes()).await {
+                Ok(_) => domain.to_string(),
+                Err(_) => "".into(),
+            },
+            Err(_) => "".into(),
         }
-    }
-    /// Destroy a previously created desktop entry.
-    async fn destroy_entry(&self, identity: &str) -> bool {
-        // TODO: Implement
-        true
     }
 }
 
-fn set_up_environment() {}
-
-#[async_std::main]
-async fn main() -> Result<()> {
-    let connection = Connection::session().await?;
+fn set_up_environment() -> Daemon {
     let daemon = Daemon {
         path: TempDir::new().expect("Could not create a temporary directory inside tmp"),
         entries: Vec::new(),
     };
+
+    return daemon;
+}
+
+#[async_std::main]
+async fn main() -> ZbusResult<()> {
+    let daemon = set_up_environment();
+    let connection = Connection::session().await?;
     // setup the server
     connection
         .object_server()
