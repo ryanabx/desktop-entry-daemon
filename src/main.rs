78d48807ed526828bf773291f4446a0bf8c0a6bd
Ryan Brue <ryanbrue@hotmail.com>
2024-04-09 11:20:40 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,41 @@ use async_std::{
     io::WriteExt,
     path::{Path, PathBuf},
 };
+use std::process::Command;
 use xdg::BaseDirectories;
 use zbus::{interface, Connection, Result as ZbusResult};
-use std::process::Command;
 
 mod utils;
 
 struct Daemon {
     data_dir: PathBuf,
-    files: Vec<tempfile::NamedTempFile>
 }
 
 #[interface(name = "net.ryanabx.XDGTempDaemon")]
 impl Daemon {
-    /// Register temporary XDG data. Requires the `path` to the data directory to copy.
+    /// Register XDG data. Requires the `path` to the data directory to copy.
     /// The data can include desktop entries, icons, etc.
     /// Entries are cleared when the daemon restarts
     async fn temp_data(&self, path: &str) -> bool {
         let data_path = Path::new(path);
-
-
-        true
-    }
-}
-
-async fn set_up_environment() -> Daemon {
-    let base_dir = BaseDirectories::new().expect("could not get XDG base directories");
-    // Find the xdg-temp-daemon directory
-    let app_dir = base_dir
-        .get_data_dirs()
-        .iter()
-        .find(|x| {
-            println!("{:?}", x);
-            x.ends_with(Path::new("xdg-temp-daemon/share"))
-        })
-        .expect("cannot find xdg-temp-daemon xdg data directory");
-    // Clear old entries (won't error if it doesn't exist)
-    let _ = fs::remove_dir_all(app_dir.clone());
-    // Create the xdg-temp-daemon directory
-    let _ = fs::create_dir_all(app_dir.clone())
-        .await
-        .expect("could not create directory");
-    Daemon {
-        data_dir: app_dir.clone().into(),
-        files: Vec::new()
+        if data_path.exists().await {
+            // TODO: validation of input
+            match utils::copy(data_path, &self.data_dir) {
+                Ok(_) => {
+                    return true;
+                }
+                Err(_) => {
+                    return false;
+                }
+            }
+        }
+        false
     }
 }
 
 #[async_std::main]
 async fn main() -> ZbusResult<()> {
-    let daemon = set_up_environment().await;
+    let daemon = utils::set_up_environment();
     let connection = Connection::session().await?;
     // setup the server
     connection
@@ -60,9 +45,7 @@ async fn main() -> ZbusResult<()> {
         .at("/net/ryanabx/XDGTempDaemon", daemon)
         .await?;
     // before requesting the name
-    connection
-        .request_name("net.ryanabx.XDGTempDaemon")
-        .await?;
+    connection.request_name("net.ryanabx.XDGTempDaemon").await?;
 
     loop {
         // do something else, wait forever or timeout here:
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
+use xdg::BaseDirectories;
+
+use crate::Daemon;
+
 pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
     let mut stack = Vec::new();
     stack.push(PathBuf::from(from.as_ref()));
@@ -50,4 +54,51 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
 
 pub fn validate_data<U: AsRef<Path>>(from: U) -> bool {
     true // TODO: Data validation
+}
+
+fn add_path_to_env_if_not_exists(variable: String, val: String) {
+    let v = std::env::var(&variable).unwrap_or("".to_string());
+    if !v.split(":").any(|x| Path::new(x) == Path::new(&val)) {
+        println!("Adding variable");
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(format!(
+                "export XDG_DATA_DIRS && XDG_DATA_DIRS=\"$XDG_DATA_DIRS:{}\"",
+                val
+            ))
+            .output()
+            .expect("could not run command");
+        std::env::set_var(&variable, format!("{}:{}", v, &val));
+        println!("{}", std::env::var(&variable).unwrap_or("".to_string()));
+    }
+}
+
+fn get_data_dir() -> PathBuf {
+    if let Ok(home) = std::env::var("HOME") {
+        let mut path = PathBuf::new();
+        path.push(&home);
+        path.push(".cache/xdg-temp-daemon/share/");
+        add_path_to_env_if_not_exists("XDG_DATA_DIRS".to_string(), path.to_str().unwrap().into());
+    }
+    let base_dir = BaseDirectories::new().expect("could not get XDG base directories");
+    let data_dirs = base_dir.get_data_dirs();
+    // Find the xdg-temp-daemon directory
+    let app_dir = data_dirs
+        .iter()
+        .find(|x| {
+            x.ends_with(Path::new(".cache/xdg-temp-daemon/share/"))
+        })
+        .expect("cannot find xdg-temp-daemon xdg data directory");
+    // Clear old entries (won't error if it doesn't exist)
+    let _ = fs::remove_dir_all(app_dir.clone());
+    // Create the xdg-temp-daemon directory
+    let _ = fs::create_dir_all(app_dir.clone()).expect("could not create directory");
+    app_dir.to_owned()
+}
+
+pub fn set_up_environment() -> Daemon {
+    let data_dir = get_data_dir();
+    Daemon {
+        data_dir: data_dir.clone().into(),
+    }
 }
