900d4ec93a83a56941846a74061445d0244e0103
Ryan Brue <ryanbrue@hotmail.com>
2024-04-11 14:55:29 -0500

    wip stuff

--- src/daemon.rs
+++ src/daemon.rs
@@ -14,12 +14,12 @@ pub struct Daemon {
 impl Daemon {
     /// register desktop application entries. each entry path should be encoded in a string in the list, and
     /// should follow the [desktop entry spec](https://specifications.freedesktop.org/desktop-entry-spec/latest/)
-    async fn register_entries(&self, entry_paths: Vec<String>) -> bool {
+    async fn register_entries(&self, entry_paths: Vec<&str>) -> bool {
         let mut success = true;
         let modified_entries = entry_paths
             .iter()
             .map(|entry| {
-                validate_desktop_entry(entry.clone()).unwrap_or_else(|| {
+                validate_desktop_entry(entry).unwrap_or_else(|| {
                     success = false;
                     ("".into(), "".into())
                 })
@@ -49,13 +49,13 @@ impl Daemon {
 
     /// register icons for applications. each icon must follow the
     /// [icon theme spec](https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html)
-    async fn register_icons(&self, icon_paths: Vec<String>) -> bool {
+    async fn register_icons(&self, icon_paths: Vec<&str>) -> bool {
         false
     }
 
     /// remove desktop application entries. use the app_id to reference the entry
     /// to delete from the desktop-entry-daemon data directory
-    async fn remove_entries(&self, entry_names: Vec<String>) -> bool {
+    async fn remove_entries(&self, entry_names: Vec<&str>) -> bool {
         let mut success = true;
         for app_id in entry_names {
             if let Err(_) = remove_file(
@@ -71,7 +71,7 @@ impl Daemon {
 
     /// remove icons. use the icon name to reference the entry
     /// to delete from the desktop-entry-daemon data directory
-    async fn remove_icons(&self, icon_names: Vec<String>) -> bool {
+    async fn remove_icons(&self, icon_names: Vec<&str>) -> bool {
         false
     }
 }
--- src/freedesktop/desktop_entry.rs
+++ src/freedesktop/desktop_entry.rs
@@ -2,14 +2,12 @@ use std::{fs::read_to_string, path::Path, process::Command};
 
 /// validate a desktop entry. takes in an entry path and returns the resulting desktop
 /// entry string and the application id
-pub fn validate_desktop_entry(entry_path: String) -> Option<(String, String)> {
-    let entry = Path::new(&entry_path);
+pub fn validate_desktop_entry(entry_path: &str) -> Option<(String, String)> {
+    let entry = Path::new(entry_path);
     // Make sure path exists
     if !entry.exists() {
         return None;
-    } else if let Some(false) | None =
-        run_desktop_file_validate(entry.to_str().unwrap().to_string())
-    {
+    } else if let Some(false) | None = run_desktop_file_validate(entry.to_str().unwrap()) {
         return None;
     }
     // TODO: Extra validation (strip exec, etc...)
@@ -29,7 +27,7 @@ pub fn validate_desktop_entry(entry_path: String) -> Option<(String, String)> {
     }
 }
 
-fn run_desktop_file_validate(entry_path: String) -> Option<bool> {
+fn run_desktop_file_validate(entry_path: &str) -> Option<bool> {
     match Command::new("sh")
         .arg("-c")
         .arg(format!("desktop-file-validate {}", entry_path))
