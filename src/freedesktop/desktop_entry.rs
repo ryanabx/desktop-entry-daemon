ca767d2c32bab73a89f63cfb1bccaca903c18ff5
Ryan Brue <ryanbrue@hotmail.com>
2024-04-11 22:15:42 -0500

    Update desktop_entry.rs

--- src/freedesktop/desktop_entry.rs
+++ src/freedesktop/desktop_entry.rs
@@ -3,6 +3,7 @@ use std::{fs::read_to_string, path::Path, process::Command};
 /// validate a desktop entry. takes in an entry path and returns the resulting desktop
 /// entry string and the application id
 pub fn validate_desktop_entry(entry_path: &str) -> Option<(String, String)> {
+    log::debug!("entry_path: {}", entry_path);
     let entry = Path::new(entry_path);
     // Make sure path exists
     if !entry.exists() {
@@ -30,12 +31,22 @@ pub fn validate_desktop_entry(entry_path: &str) -> Option<(String, String)> {
 fn run_desktop_file_validate(entry_path: &str) -> Option<bool> {
     match Command::new("sh")
         .arg("-c")
-        .arg(format!("desktop-file-validate {}", entry_path))
+        .arg(format!(
+            "desktop-file-validate --no-hints --no-warn-deprecated {}",
+            entry_path
+        ))
         .output()
     {
         Ok(output) => {
-            println!("Output: '{}'", String::from_utf8(output.stdout).unwrap());
-            Some(true)
+            let stdout = String::from_utf8(output.stdout).unwrap();
+            let stderr = String::from_utf8(output.stderr).unwrap();
+            log::debug!(
+                "stdout: '{}', stderr: '{}' stdout.is_empty(): {}",
+                stdout,
+                stderr,
+                stdout.is_empty()
+            );
+            Some(stdout.is_empty())
         }
         Err(_) => None,
     }
