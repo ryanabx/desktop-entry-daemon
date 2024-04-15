01581afa646dfc378d27452f3060470476258052
Ryan Brue <ryanbrue@hotmail.com>
2024-04-15 10:14:54 -0500

    update profile.d

--- profile/desktop-entry-daemon.sh
+++ profile/desktop-entry-daemon.sh
@@ -1,3 +1,3 @@
 # set XDG_DATA_DIRS to include desktop-entry-daemon installations
 export XDG_DATA_DIRS
-XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.cache/desktop-entry-daemon/share/"
+XDG_DATA_DIRS="$XDG_DATA_DIRS:$XDG_RUNTIME_DIR/desktop-entry-daemon/"
